6202530caff29339771a81a8a61cbec200f6d911
Nithin Pranesh <nithinpra7@gmail.com>
2023-04-11 23:09:01 -0400

    Basic cached playback working

--- Include/SolverNode.h
+++ Include/SolverNode.h
@@ -1,33 +1,47 @@
 #pragma once
 
 #include <Pies/Solver.h>
-
-#include <maya/MStatus.h>
-#include <maya/MPxNode.h>
 #include <maya/MFnTypedAttribute.h>
 #include <maya/MFnUnitAttribute.h>
 #include <maya/MObject.h>
-#include <maya/MTypeId.h>
+#include <maya/MPxNode.h>
+#include <maya/MStatus.h>
 #include <maya/MString.h>
+#include <maya/MTypeId.h>
 
-#include <string>
 #include <memory>
+#include <string>
 
 // TODO: Rename to PiesSolvernode
 class SolverNode : public MPxNode {
 public:
-	SolverNode();
-	virtual MStatus compute(const MPlug& plug, MDataBlock& data);
-	static void* creator();
-	static MStatus initialize();
+  SolverNode();
+  MStatus compute(const MPlug& plug, MDataBlock& data) override;
+  void getCacheSetup(
+      const MEvaluationNode& evaluationNode,
+      MNodeCacheDisablingInfo& disablingInfo,
+      MNodeCacheSetupInfo& setupInfo,
+      MObjectArray& monitoredAttributes) const override;
+  void configCache(const MEvaluationNode& evalNode, MCacheSchema& schema) const;
+  MTimeRange transformInvalidationRange(
+      const MPlug& source,
+      const MTimeRange& input) const override;
 
-	static MString pluginDir;
-	static MTypeId id;
-	static MObject time;
-	static MObject stepSize;
-	static MObject iterations;
-	static MObject ouptutPositions;
+  static void* creator();
+  static MStatus initialize();
+
+  static MString pluginDir;
+  static MTypeId id;
+  static MObject time;
+  static MObject prevTime;
+  static MObject stepSize;
+  static MObject iterations;
+  static MObject simulationEnabled;
+  static MObject simulationStartTime;
+
+  static MObject ouptutPositions;
 
 private:
-  static std::unique_ptr<Pies::Solver> _pSolver;
+  std::unique_ptr<Pies::Solver> _pSolver;
+  float _simulationTime = 0.0f;
 };
--- Src/SolverNode.cpp
+++ Src/SolverNode.cpp
@@ -11,6 +11,7 @@
 #include <maya/MGlobal.h>
 #include <maya/MIOStream.h>
 #include <maya/MIntArray.h>
+#include <maya/MNodeCacheDisablingInfoHelper.h>
 #include <maya/MPoint.h>
 #include <maya/MPointArray.h>
 #include <maya/MStatus.h>
@@ -37,43 +38,47 @@ MTypeId SolverNode::id(0x00000001);
 MObject SolverNode::time;
 
 /*static*/
+MObject SolverNode::prevTime;
+
+/*static*/
 MObject SolverNode::stepSize;
 
 /*static*/
 MObject SolverNode::iterations;
 
 /*static*/
-MObject SolverNode::ouptutPositions;
+MObject SolverNode::simulationEnabled;
 
 /*static*/
-std::unique_ptr<Pies::Solver> SolverNode::_pSolver = nullptr;
+MObject SolverNode::simulationStartTime;
+
+/*static*/
+MObject SolverNode::ouptutPositions;
 
 SolverNode::SolverNode() {
-  if (!this->_pSolver) {
-    Pies::SolverOptions options{};
-    options.gridSpacing = 1.0f;
-    options.floorHeight = -8.0f;
+  Pies::SolverOptions options{};
+  options.gridSpacing = 1.0f;
+  options.floorHeight = -8.0f;
 
-    SolverNode::_pSolver = std::make_unique<Pies::Solver>(options);
-    SolverNode::_pSolver->createTetBox(
-        glm::vec3(0.0f),
-        1.0f,
-        glm::vec3(0.0f),
-        1000.0f,
-        1.0f,
-        true);
-  }
+  this->_pSolver = std::make_unique<Pies::Solver>(options);
+  this->_pSolver->createTetBox(
+      glm::vec3(0.0f),
+      1.0f,
+      glm::vec3(0.0f),
+      1000.0f,
+      1.0f,
+      true);
 }
 
 MStatus SolverNode::compute(const MPlug& plug, MDataBlock& data) {
   MStatus status = MStatus::kSuccess;
 
-  // TODO: this is a hack
-  SolverNode::_pSolver->tick(0.012f);
-
   if (plug == ouptutPositions) {
     MDataHandle outputHandle = data.outputValue(ouptutPositions, &status);
     McheckErr(status, "ERROR getting output positions handle.");
+
+    MDataHandle outputPrevTimeHandle = data.outputValue(prevTime, &status);
+    McheckErr(status, "ERROR getting output prevTime handle.");
 
     MDataHandle stepSizeHandle = data.inputValue(stepSize, &status);
     McheckErr(status, "ERROR getting step size handle.");
@@ -84,35 +89,63 @@ MStatus SolverNode::compute(const MPlug& plug, MDataBlock& data) {
     MDataHandle timeHandle = data.inputValue(time, &status);
     McheckErr(status, "ERROR getting time handle.");
 
-    double timeSeconds = timeHandle.asTime().asUnits(MTime::Unit::kSeconds);
+    MDataHandle prevTimeHandle = data.inputValue(prevTime, &status);
+    McheckErr(status, "ERROR getting prevTime handle.");
 
+    MDataHandle simulationEnabledHandle =
+        data.inputValue(simulationEnabled, &status);
+    McheckErr(status, "ERROR getting simulationEnabledHandle handle.");
+
+    MTime currentTime = timeHandle.asTime();
+    const MTime previousTime = prevTimeHandle.asTime();
+
+    // if (currentTime == previousTime) {
+    //   // TODO: ??
+    //   data.setClean(ouptutPositions);
+    //   return MStatus::kSuccess;
+    // }
+
+    double timeSeconds = currentTime.asUnits(MTime::Unit::kSeconds);
+
+    bool simulationEnabledValue = simulationEnabledHandle.asBool();
     float currentStepSize = stepSizeHandle.asFloat();
     int currentIters = itersHandle.asInt();
 
-    MFnArrayAttrsData particlesAAD;
-    MObject particlesObj = particlesAAD.create();
-    MVectorArray particlesPosArr = particlesAAD.vectorArray("position");
-    MVectorArray particlesScaleArr = particlesAAD.vectorArray("scale");
-    MDoubleArray particlesIdArr = particlesAAD.doubleArray("id");
+    if (simulationEnabledValue && this->_simulationTime < timeSeconds) {
+      // TODO: Is this correct??
+      this->_pSolver->tick(0.012f);
+      this->_simulationTime = timeSeconds;
 
-    const std::vector<Pies::Solver::Vertex> piesVerts =
-        SolverNode::_pSolver->getVertices();
+      MFnArrayAttrsData particlesAAD;
+      MObject particlesObj = particlesAAD.create();
+      MVectorArray particlesPosArr = particlesAAD.vectorArray("position");
+      MVectorArray particlesScaleArr = particlesAAD.vectorArray("scale");
+      MDoubleArray particlesIdArr = particlesAAD.doubleArray("id");
 
-    particlesPosArr.setLength(piesVerts.size());
-    particlesScaleArr.setLength(piesVerts.size());
-    particlesIdArr.setLength(piesVerts.size());
+      const std::vector<Pies::Solver::Vertex> piesVerts =
+          this->_pSolver->getVertices();
 
-    for (size_t i = 0; i < piesVerts.size(); ++i) {
-      const Pies::Solver::Vertex& vertex = piesVerts[i];
+      particlesPosArr.setLength(piesVerts.size());
+      particlesScaleArr.setLength(piesVerts.size());
+      particlesIdArr.setLength(piesVerts.size());
 
-      particlesIdArr[i] = static_cast<double>(i);
-      particlesPosArr[i] =
-          MVector(vertex.position.x, vertex.position.y, vertex.position.z);
-      particlesScaleArr[i] = 
-          MVector(vertex.radius, vertex.radius, vertex.radius);
+      for (size_t i = 0; i < piesVerts.size(); ++i) {
+        const Pies::Solver::Vertex& vertex = piesVerts[i];
+
+        particlesIdArr[i] = static_cast<double>(i);
+        particlesPosArr[i] =
+            MVector(vertex.position.x, vertex.position.y, vertex.position.z);
+        particlesScaleArr[i] =
+            MVector(vertex.radius, vertex.radius, vertex.radius);
+      }
+
+      outputHandle.setMObject(particlesObj);
     }
 
-    outputHandle.setMObject(particlesObj);
+    data.setClean(ouptutPositions);
+
+    outputPrevTimeHandle.setMTime(currentTime);
+    data.setClean(prevTime);
 
     data.setClean(plug);
 
@@ -120,6 +153,94 @@ MStatus SolverNode::compute(const MPlug& plug, MDataBlock& data) {
   }
 
   return MStatus::kUnknownParameter;
+}
+
+void SolverNode::getCacheSetup(
+    const MEvaluationNode& evaluationNode,
+    MNodeCacheDisablingInfo& disablingInfo,
+    MNodeCacheSetupInfo& setupInfo,
+    MObjectArray& monitoredAttributes) const {
+  // Check if the attribute is animated.  If it is animated, requestSimulation
+  // will be true so that the behavior is the same as if the scene was played
+  // back from the beginning of the time range.  That being said, it makes very
+  // little sense to animate whether the simulation is active or not.
+  const bool requestSimulation =
+      MNodeCacheDisablingInfoHelper::testBooleanAttribute(
+          nullptr,
+          monitoredAttributes,
+          evaluationNode,
+          simulationEnabled,
+          false);
+
+  if (requestSimulation) {
+    // Request simulation support
+    setupInfo.setRequirement(MNodeCacheSetupInfo::kSimulationSupport, true);
+		setupInfo.setPreference(MNodeCacheSetupInfo::kWantToCacheByDefault, true);
+  }
+}
+
+void SolverNode::configCache(
+    const MEvaluationNode& evalNode,
+    MCacheSchema& schema) const {
+  if (evalNode.dirtyPlugExists(ouptutPositions)) {
+    schema.add(ouptutPositions);
+  }
+}
+
+MTimeRange SolverNode::transformInvalidationRange(
+    const MPlug& source,
+    const MTimeRange& input) const {
+  // Never call the parent class method, it's not meant to be called from the
+  // base class, the implementation is only used to detect whether there is an
+  // override or not.
+
+  // When setting infinite values, do not use max()/min() directly, could
+  // overflow easily.
+  static constexpr MTime::MTick kMaximumTimeTick =
+      std::numeric_limits<MTime::MTick>::max() / 2;
+  static constexpr MTime::MTick kMinimumTimeTick =
+      std::numeric_limits<MTime::MTick>::min() / 2 + 1;
+  static const MTime kMaximumTime{
+      kMaximumTimeTick / static_cast<double>(MTime::ticksPerSecond()),
+      MTime::kSeconds};
+  static const MTime kMinimumTime{
+      kMinimumTimeTick / static_cast<double>(MTime::ticksPerSecond()),
+      MTime::kSeconds};
+
+  // Get the start time and whether simulation is enabled., but it should NOT be
+  // animated, so it should be clean.
+  MDataBlock data = const_cast<SolverNode*>(this)->forceCache();
+  if (!data.isClean(simulationStartTime) || !data.isClean(simulationEnabled)) {
+    return MTimeRange{kMinimumTime, kMaximumTime};
+  }
+
+  MStatus returnStatus;
+  // Whether the simulation is enabled or not is clean, get its value.
+  MDataHandle simulationEnabledData =
+      data.inputValue(simulationEnabled, &returnStatus);
+  const bool simulationEnabled =
+      returnStatus ? simulationEnabledData.asBool() : true;
+  if (!simulationEnabled) {
+    return input;
+  }
+
+  // The start time is clean, get its value.
+  MDataHandle simulationStartTimeData =
+      data.inputValue(simulationStartTime, &returnStatus);
+  const MTime simulationStartTime =
+      returnStatus ? simulationStartTimeData.asTime() : kMinimumTime;
+  const MTime simulationEndTime = kMaximumTime;
+
+  if (input.intersects(simulationStartTime, simulationEndTime)) {
+    // We invalidate the whole simulation time range, because something in the
+    // range was invalidated, therefore we have to recompute the whole
+    // simulation.
+    return input | MTimeRange{simulationStartTime, simulationEndTime};
+  } else {
+    // Since the invalidation range does not touch the simulation,
+    // We don't need to invalidate anything.
+    return MTimeRange{};
+  }
 }
 
 /*static*/
@@ -135,8 +256,23 @@ MStatus SolverNode::initialize() {
   McheckErr(status, "ERROR creating attribute SolverNode::time.");
 
   timeAttr.setWritable(true);
+  timeAttr.setStorable(true);
+  timeAttr.setReadable(true);
+  timeAttr.setKeyable(true);
   status = addAttribute(SolverNode::time);
   McheckErr(status, "ERROR adding attribute SolverNode::time.");
+
+  MFnUnitAttribute prevTimeAttr;
+  SolverNode::prevTime =
+      prevTimeAttr
+          .create("prevTime", "prevT", MFnUnitAttribute::kTime, 0.0, &status);
+  McheckErr(status, "ERROR creating attribute SolverNode::prevTime.");
+
+  prevTimeAttr.setWritable(false);
+  prevTimeAttr.setStorable(false);
+  prevTimeAttr.setHidden(true);
+  status = addAttribute(SolverNode::prevTime);
+  McheckErr(status, "ERROR adding attribute SolverNode::prevTime.");
 
   MFnNumericAttribute stepSizeAttr;
   SolverNode::stepSize =
@@ -157,6 +293,35 @@ MStatus SolverNode::initialize() {
   status = addAttribute(SolverNode::iterations);
   McheckErr(status, "ERROR adding attribute SolverNode::iterations.");
 
+  MFnNumericAttribute simulationEnabledAttr;
+  SolverNode::simulationEnabled = simulationEnabledAttr.create(
+      "simulationEnabled",
+      "enable",
+      MFnNumericData::kBoolean,
+      true,
+      &status);
+  McheckErr(status, "ERROR creating attribute SolverNode::simulationEnabled.");
+
+  simulationEnabledAttr.setWritable(true);
+  simulationEnabledAttr.setKeyable(false);
+  status = addAttribute(SolverNode::simulationEnabled);
+  McheckErr(status, "ERROR adding attribute SolverNode::simulationEnabled.");
+
+  MFnNumericAttribute simulationStartTimeAttr;
+  SolverNode::simulationStartTime = simulationStartTimeAttr.create(
+      "simulationStartTime",
+      "start",
+      MFnNumericData::kFloat,
+      0.0,
+      &status);
+  McheckErr(
+      status,
+      "ERROR creating attribute SolverNode::simulationStartTime.");
+
+  simulationStartTimeAttr.setWritable(true);
+  status = addAttribute(SolverNode::simulationStartTime);
+  McheckErr(status, "ERROR adding attribute SolverNode::simulationStartTime.");
+
   MFnTypedAttribute outputPositionsAttr;
   SolverNode::ouptutPositions = outputPositionsAttr.create(
       "outputPositions",
@@ -169,6 +334,7 @@ MStatus SolverNode::initialize() {
   outputPositionsAttr.setReadable(true);
   outputPositionsAttr.setWritable(false);
   outputPositionsAttr.setStorable(true);
+  outputPositionsAttr.setCached(true);
   status = addAttribute(SolverNode::ouptutPositions);
   McheckErr(status, "ERROR adding attribute SolverNode::ouptutPositions.");
 
@@ -181,6 +347,16 @@ MStatus SolverNode::initialize() {
 
   status = attributeAffects(iterations, ouptutPositions);
   McheckErr(status, "ERROR attributeAffects(iterations, ouptutPositions).");
+
+  status = attributeAffects(simulationEnabled, ouptutPositions);
+  McheckErr(
+      status,
+      "ERROR attributeAffects(simulationEnabled, ouptutPositions).");
+
+  status = attributeAffects(simulationStartTime, ouptutPositions);
+  McheckErr(
+      status,
+      "ERROR attributeAffects(simulationStartTime, ouptutPositions).");
 
   return status;
 }
