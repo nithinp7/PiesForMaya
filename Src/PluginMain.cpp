07a2b8dd1e5ecf4ccf2c659bd4a7e9078b5b3d2f
AKR <raiaboudi@yahoo.com>
2023-04-25 22:01:01 -0400

    added basic pies menu

--- Src/PluginMain.cpp
+++ Src/PluginMain.cpp
@@ -34,10 +34,10 @@ MStatus initializePlugin(MObject obj) {
   std::string pluginPath(SolverNode::pluginDir.asChar());
   // TODO: Add scripts from folder
 
-  // std::string scriptPath = pluginPath + "/../../LSystemMenu.mel";
-  // std::string addScriptPathCmd = "source \"" + scriptPath + "\";";
+  std::string scriptPath = pluginPath + "/../PiesMenu.mel";
+  std::string addScriptPathCmd = "source \"" + scriptPath + "\";";
 
-  // MGlobal::executeCommand(MString(addScriptPathCmd.c_str()));
+  MGlobal::executeCommand(MString(addScriptPathCmd.c_str()));
 
   status = plugin.registerNode(
       "SolverNode",
